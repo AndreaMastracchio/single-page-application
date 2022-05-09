1c3786b73365d5bfeeac4a66c0461388a08eacb7
Andrea Gregorio Mastracchio <66825008+AndreaMastracchio@users.noreply.github.com>
2022-05-09 08:19:58 +0200

    Gitignore

--- configuration.js
+++
@@ -1,7 +0,0 @@
-export const options = {
-    endpoint: URL,
-    method: "GET",
-    headers: {
-        "X-Shopify-Access-Token": process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN
-    }
-}
--- pages/api/collection_listing.tsx
+++ pages/api/collection_listing.tsx
@@ -3,8 +3,6 @@
 export default async function handler(req, res) {
 
     const URL = `https://pippolotto.myshopify.com/admin/api/2022-04/collection_listings.json`
-    // const URL_1 = 'https://pippolotto.myshopify.com/admin/api/2022-04/collections/266329686089/products.json'
-    // const URL_2 = 'https://pippolotto.myshopify.com/admin/api/2022-04/products/'+req.product+'.json'
         
     const options = {
         endpoint: URL,
@@ -15,9 +13,7 @@ export default async function handler(req, res) {
     }
 
     const shopy_collection = await fetch(URL, options)
-    // const product_collection_await = await fetch(URL_1, options)
     const data = await shopy_collection.json()
-    // const product_collection = await product_collection_await.json()
 
     res.status(200).json({ data: data })
 }
